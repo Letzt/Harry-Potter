cedcae9314559354769bf8761f9301dcc5c0edb7
Maanu <47236863+Letzt@users.noreply.github.com>
2023-04-09 03:29:19 -0400

    Shooter rotate level 3

---
+++ scripts/Wand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wand : MonoBehaviour
+{
+
+    public float speed = 10f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+        transform.Rotate(0, x, 0);
+    }
+}
