e4998d28753150a86e9e33d367175bb440b75b66
Maanu <47236863+Letzt@users.noreply.github.com>
2023-04-09 03:28:37 -0400

    Screen change

---
+++ scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public bool pause;
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    private void Start()
+    {
+        pause = false;
+    }
+    public void OnPause()
+    {
+        pause = !pause;
+        if(!pause)
+        {
+            Time.timeScale = 1;
+        }
+        else if(pause)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
