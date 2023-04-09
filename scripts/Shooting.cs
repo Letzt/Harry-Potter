0b5dfb6371555bdf2986983fddcdca4095c318a0
Maanu <47236863+Letzt@users.noreply.github.com>
2023-04-09 03:29:48 -0400

    Shooting script

---
+++ scripts/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+    float bulletSpeed = 1100f;
+    public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Fire () {
+        GameObject tempBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+        Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
+        tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+        Destroy(tempBullet, 0.5f);
+        GetComponent<AudioSource>().Play();
+    }
+     void Update()
+    {
+    if(Input.GetMouseButtonDown(0))
+        { Fire(); }
+    }
+}
