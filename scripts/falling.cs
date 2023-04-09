c027fec5a292679aead5cb762b1b84800d350f92
Maanu <47236863+Letzt@users.noreply.github.com>
2023-04-09 03:26:00 -0400

    Initial script for falling rocks

---
+++ scripts/falling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falling : MonoBehaviour {
+
+    Vector3 temppos;
+    public static float t = 0.0f;
+    float y;
+    // Use this for initialization
+    void Start ()
+    { 
+        y = Random.Range(0.05f, 0.1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        temppos = transform.position;
+        temppos.y -= y;
+        if (temppos.y <= 0f)
+        {
+            temppos.y = 17.5f;
+        }
+        transform.position = temppos;
+    }
+}
